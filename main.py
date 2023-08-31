7f03879cc172abe708434eacc11e9abc7baccd37
namnguyenvu1 <126105726+namnguyenvu1@users.noreply.github.com>
2023-08-31 10:28:08 -0600

    Create function
    Create function

--- main.py
+++ main.py
@@ -1,16 +1,523 @@
-# This is a sample Python script.
+import sklearn
+from sklearn.datasets import make_circles
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Make 1000 samples
+n_samples = 1000
+
+# Create circle
+X, y = make_circles(n_samples,
+                   noise=0.03,
+                   random_state=42) # Create circle data
+len(X), len(y)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+print(f"First 5 of X: {X[:5]}")
+print(f"First 5 of y: {y[:5]}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Make DataFrame of circle data
+import pandas as pd
+circles = pd.DataFrame({"X1": X[:, 0],
+                        "X2": X[:, 1],
+                        "label": y})
+circles.head(10)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Visualize our data
+import matplotlib.pyplot as plt
+plt.scatter(x=X[:, 0],
+            y=X[:, 1],
+            c=y,
+            cmap=plt.cm.RdYlBu)
+
+X.shape, y.shape
+
+
+# View the first example of features and labels
+X_sample = X[0]
+y_sample = y[0]
+
+print(f"Values for one sample of X: {X_sample} and the same for y: {y_sample}")
+print(f"Shapes for one sample of X: {X_sample.shape} and the same for y: {y_sample.shape}")
+
+
+import torch
+torch.__version__
+
+
+
+# Turn data into tensors
+X = torch.from_numpy(X).type(torch.float)
+y = torch.from_numpy(y).type(torch.float)
+
+
+# Split data into training and test sets
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2, # 20% of data will be test & 80% will be train
+                                                    random_state=42)
+
+
+len(X_train), len(X_test), len(y_train), len(y_test)
+
+
+# Import PyTorch and nn
+import torch
+from torch import nn
+
+# Make device agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+device
+
+
+import requests
+from pathlib import Path
+
+# Download helper functions from Learn PyTorch repo (if not already downloaded)
+if Path("helper_functions.py").is_file():
+  print("helper_functions.py already exists, skipping download")
+else:
+  print("Download helper_function.py")
+  request = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+  with open("helper_functions.py", "wb") as f:
+    f.write(request.content)
+
+from helper_functions import plot_predictions, plot_decision_boundary
+
+
+
+# 1. Contruct a model that subclasses nn.Module
+class CircleModelV0(nn.Module):
+  def __init__(self):
+    super().__init__()
+    # 2. Create 2 nn.Linear layers capable of handling the shapes of our data
+    self.layer_1 = nn.Linear(in_features=2, out_features=5) # Shift + command + space to find the parameters need to pass
+    self.layer_2 = nn.Linear(in_features=5, out_features=1)
+
+  # 3. Define a forward method containing the forward pass computation
+  def forward(self, x):
+    return self.layer_2(self.layer_1(x))  # x -> layer_1 -> layer_2 -> output
+
+# 4. Instantiate an instance of our model class and send it to the target device
+model_0 = CircleModelV0().to(device)
+model_0
+
+
+# Replicate CircleModelV0 with nn.Sequential (Another way to create the previous neural network)
+model_0 = nn.Sequential(
+    nn.Linear(in_features=2, out_features=5),
+    nn.Linear(in_features=5, out_features=1)
+).to(device)
+
+model_0
+
+
+# Setup the loss function
+# loss_fn = nn.BCELoss()  # BCELoss = requires inputs to have gone through
+loss_fn = nn.BCEWithLogitsLoss() # BCEWithLogitsLoss = sigmoid built-in
+
+# Create the optimizer
+optimizer = torch.optim.SGD(params=model_0.parameters(),
+                            lr=0.1)
+# Calculate accuracy - out of 100 examples, what percentage does our model get right?
+def accuracy_fn(y_true, y_pred):
+  correct = torch.eq(y_true, y_pred).sum().item()
+  acc = (correct/len(y_pred)) * 100
+  return acc
+
+
+# View the first 5 outputs of the forward pass on the test data
+model_0.eval()
+with torch.inference_mode():
+  y_logits = model_0(X_test.to(device))[:5]
+y_logits
+
+
+# Use the sigmoid activation
+y_pred_probs = torch.sigmoid(y_logits)
+y_pred_probs
+
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+# Set the number of epochs
+epochs = 100
+
+# Put data to target device
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+
+# Build training and evaluation loop
+for epoch in range(epochs):
+    ### Training
+    model_0.train()
+
+    # 1. Forward pass (model outputs raw logits)
+    y_logits = model_0(X_train).squeeze() # squeeze to remove extra `1` dimensions, this won't work unless model and data are on same device
+    y_pred = torch.round(torch.sigmoid(y_logits)) # turn logits -> pred probs -> pred labls
+
+    # 2. Calculate loss/accuracy
+    # loss = loss_fn(torch.sigmoid(y_logits), # Using nn.BCELoss you need torch.sigmoid()
+    #                y_train)
+    loss = loss_fn(y_logits, # Using nn.BCEWithLogitsLoss works with raw logits
+                   y_train)
+    acc = accuracy_fn(y_true=y_train,
+                      y_pred=y_pred)
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backwards
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    ### Testing
+    model_0.eval()
+    with torch.inference_mode():
+        # 1. Forward pass
+        test_logits = model_0(X_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+        # 2. Caculate loss/accuracy
+        test_loss = loss_fn(test_logits,
+                            y_test)
+        test_acc = accuracy_fn(y_true=y_test,
+                               y_pred=test_pred)
+
+    # Print out what's happening every 10 epochs
+    if epoch % 10 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
+
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+
+    # Set the number of epochs
+    epochs = 100
+
+    # Put data to target device
+    X_train, y_train = X_train.to(device), y_train.to(device)
+    X_test, y_test = X_test.to(device), y_test.to(device)
+
+    # Build training and evaluation loop
+    for epoch in range(epochs):
+        ### Training
+        model_0.train()
+
+        # 1. Forward pass (model outputs raw logits)
+        y_logits = model_0(
+            X_train).squeeze()  # squeeze to remove extra `1` dimensions, this won't work unless model and data are on same device
+        y_pred = torch.round(torch.sigmoid(y_logits))  # turn logits -> pred probs -> pred labls
+
+        # 2. Calculate loss/accuracy
+        # loss = loss_fn(torch.sigmoid(y_logits), # Using nn.BCELoss you need torch.sigmoid()
+        #                y_train)
+        loss = loss_fn(y_logits,  # Using nn.BCEWithLogitsLoss works with raw logits
+                       y_train)
+        acc = accuracy_fn(y_true=y_train,
+                          y_pred=y_pred)
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backwards
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+        ### Testing
+        model_0.eval()
+        with torch.inference_mode():
+            # 1. Forward pass
+            test_logits = model_0(X_test).squeeze()
+            test_pred = torch.round(torch.sigmoid(test_logits))
+            # 2. Caculate loss/accuracy
+            test_loss = loss_fn(test_logits,
+                                y_test)
+            test_acc = accuracy_fn(y_true=y_test,
+                                   y_pred=test_pred)
+
+        # Print out what's happening every 10 epochs
+        if epoch % 10 == 0:
+            print(
+                f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
+
+
+# Create some data
+weight = 0.7
+bias = 0.3
+start = 0
+end = 1
+step = 0.01
+
+# Create data
+X_regression = torch.arange(start, end, step).unsqueeze(dim=1)
+y_regression = weight * X_regression + bias
+
+# Check the data
+print(len(X_regression))
+X_regression[:5], y_regression[:5]
+
+
+# Create train and test splits
+train_split = int(0.8 * len(X_regression)) # 80% of data used for training set
+X_train_regression, y_train_regression = X_regression[:train_split], y_regression[:train_split]
+X_test_regression, y_test_regression = X_regression[train_split:], y_regression[train_split:]
+
+# Check the lengths of each split
+print(len(X_train_regression),
+    len(y_train_regression),
+    len(X_test_regression),
+    len(y_test_regression))
+
+
+class CircleModelV1(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.layer_1 = nn.Linear(in_features=2, out_features=10)
+    self.layer_2 = nn.Linear(in_features=10, out_features=10)
+    self.layer_3 = nn.Linear(in_features=10, out_features=1)
+
+  def forward(self, x):
+    # z = self.layer_1(x)
+    # z = self.layer_2(z)
+    # z = self.layer_3(z)
+    # return z
+    return self.layer_3(self.layer_2(self.layer_1(x)))
+
+model_1 = CircleModelV1().to(device)
+model_1
+
+
+# Create a loss function
+loss_fn = nn.BCEWithLogitsLoss()
+
+# Create an optimizer
+optimizer = torch.optim.SGD(params=model_1.parameters(),
+                            lr=0.1)
+
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+epochs = 1000 # Train for longer
+
+# Put data to target device
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+
+for epoch in range(epochs):
+    ### Training
+    # 1. Forward pass
+    y_logits = model_1(X_train).squeeze()
+    y_pred = torch.round(torch.sigmoid(y_logits)) # logits -> predicition probabilities -> prediction labels
+
+    # 2. Calculate loss/accuracy
+    loss = loss_fn(y_logits, y_train)
+    acc = accuracy_fn(y_true=y_train,
+                      y_pred=y_pred)
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backwards
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    ### Testing
+    model_1.eval()
+    with torch.inference_mode():
+        # 1. Forward pass
+        test_logits = model_1(X_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+        # 2. Caculate loss/accuracy
+        test_loss = loss_fn(test_logits,
+                            y_test)
+        test_acc = accuracy_fn(y_true=y_test,
+                               y_pred=test_pred)
+
+    # Print out what's happening every 10 epochs
+    if epoch % 100 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
+
+
+# Same architecture as model_1 (but using nn.Sequential)
+model_2 = nn.Sequential(
+    nn.Linear(in_features=1, out_features=10),
+    nn.Linear(in_features=10, out_features=10),
+    nn.Linear(in_features=10, out_features=1)
+).to(device)
+
+model_2
+
+
+# Loss and optimizer
+loss_fn = nn.L1Loss()
+optimizer = torch.optim.SGD(model_2.parameters(), lr=0.1)
+
+
+# Train the model
+torch.manual_seed(42)
+
+# Set the number of epochs
+epochs = 1000
+
+# Put data to target device
+X_train_regression, y_train_regression = X_train_regression.to(device), y_train_regression.to(device)
+X_test_regression, y_test_regression = X_test_regression.to(device), y_test_regression.to(device)
+
+for epoch in range(epochs):
+    ### Training
+    # 1. Forward pass
+    y_pred = model_2(X_train_regression)
+
+    # 2. Calculate loss (no accuracy since it's a regression problem, not classification)
+    loss = loss_fn(y_pred, y_train_regression)
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backwards
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    ### Testing
+    model_2.eval()
+    with torch.inference_mode():
+      # 1. Forward pass
+      test_pred = model_2(X_test_regression)
+      # 2. Calculate the loss
+      test_loss = loss_fn(test_pred, y_test_regression)
+
+    # Print out what's happening
+    if epoch % 100 == 0:
+        print(f"Epoch: {epoch} | Train loss: {loss:.5f}, Test loss: {test_loss:.5f}")
+
+
+# Make and plot data
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_circles
+
+n_samples = 1000
+
+X, y = make_circles(n_samples,
+                    noise=0.03,
+                    random_state=42)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdYlBu);
+
+
+# Convert to tensors and split into train and test sets
+import torch
+from sklearn.model_selection import train_test_split
+
+# Turn data into tensors
+X = torch.from_numpy(X).type(torch.float)
+y = torch.from_numpy(y).type(torch.float)
+
+# Split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    test_size=0.2,
+                                                    random_state=42
+)
+
+X_train[:5], y_train[:5]
+
+
+# Build a model with non-linear activation functions
+from torch import nn
+class CircleModelV2(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.layer_1 = nn.Linear(in_features=2, out_features=10)
+    self.layer_2 = nn.Linear(in_features=10, out_features=10)
+    self.layer_3 = nn.Linear(in_features=10, out_features=1)
+    self.relu = nn.ReLU()
+
+  def forward(self, x):
+    return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
+
+model_3 = CircleModelV2().to(device)
+model_3
+
+
+# Setup loss and optimizer
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(model_3.parameters(),
+                            lr=0.1)
+
+
+# Fit the model
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+epochs = 1000
+
+# Put all data on target device
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+
+for epoch in range(epochs):
+  ### Training
+  model_3.train()
+
+  # 1. Forward pass
+  y_logits = model_3(X_train).squeeze()
+  y_pred = torch.round(torch.sigmoid(y_logits)) # logits -> prediction probabilities -> prediction labels
+  # Remember y_pred not y_preds (y_preds will make Accuracy (not test Accuracy) to 0%)
+
+  # 2. Calculate loss and accuracy
+  loss = loss_fn(y_logits, y_train) # BCEWithLogitsLoss calculates loss using logits
+  acc = accuracy_fn(y_true=y_train,
+                      y_pred=y_pred)
+
+  # 3. Optimizer zero grad
+  optimizer.zero_grad()
+
+  # 4. Loss backward
+  loss.backward()
+
+  # 5. Optimizer step
+  optimizer.step()
+
+  ### Testing
+  model_3.eval()
+  with torch.inference_mode():
+    test_logits = model_3(X_test).squeeze() # Without .squeeze(), it will cause mismatch here loss_fn(test_logits, y_test)
+    test_pred = torch.round(torch.sigmoid(test_logits))
+
+    test_loss = loss_fn(test_logits, y_test)
+    test_acc = accuracy_fn(y_true=y_test,
+                           y_pred=test_pred)
+
+  # Print out what's happening
+    if epoch % 100 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Accuracy: {test_acc:.2f}%")
+
+    # Makes predictions
+    model_3.eval()
+    with torch.inference_mode():
+        y_preds = torch.round(torch.sigmoid(model_3(X_test))).squeeze()
+    y_preds[:10], y_test[:10]
+
+    # Plot decision boundaries for training and test sets
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.title("Train")
+    plot_decision_boundary(model_1, X_train, y_train)  # model_1 = no non-linearity
+    plt.subplot(1, 2, 2)
+    plt.title("Test")
+    plot_decision_boundary(model_3, X_test, y_test)  # model_3 = has non-linearity
+
+
+
+
